7df90e4695b6b97e9566e973391ff1c12686ef3d
Stefan Hagen <stefan@hagen.link>
2022-07-10 07:58:53 +0200

    Pleased the linter

--- test/test_mglw.py
+++ test/test_mglw.py
@@ -1,9 +1,11 @@
-import moderngl_window as mglw
+import moderngl_window as mglw  # type:ignore
 
-class Test(mglw.WindowConfig):
+
+class Test(mglw.WindowConfig):  # type: ignore
     gl_version = (3, 3)
 
-    def render(self, time, frametime):
+    def render(self, time, frametime):  # type: ignore
         self.ctx.clear(1.0, 0.0, 0.0, 0.0)
+
 
 Test.run()
